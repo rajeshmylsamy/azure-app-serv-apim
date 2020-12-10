c7bc9174825e5bc66c9ebf9a8bb472a0edf90bc6
r0m09bd <rajesh.mylsamy@gmail.com>
2020-12-10 19:23:50 +0530

    added comment

--- src/main/java/com/learn/azure/azureappservapim/AzureAppServApimApplication.java
+++ src/main/java/com/learn/azure/azureappservapim/AzureAppServApimApplication.java
@@ -3,6 +3,8 @@ package com.learn.azure.azureappservapim;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//test
 @SpringBootApplication
 public class AzureAppServApimApplication {
 
